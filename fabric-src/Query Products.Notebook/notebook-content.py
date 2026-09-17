648f9ae5a1f7e45c9fc89a39878a5c89a2b08b76
saverioruffo04-stack <saverioruffo04@gmail.com>
2026-09-17 17:31:54 +0200

    Committing 1 item from workspace 8d20343b-56af-4125-b338-3e5e936018e9

---
+++ fabric-src/Query Products.Notebook/notebook-content.py
@@ -0,0 +1,32 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": []
+# META     },
+# META     "warehouse": {
+# META       "known_warehouses": []
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
