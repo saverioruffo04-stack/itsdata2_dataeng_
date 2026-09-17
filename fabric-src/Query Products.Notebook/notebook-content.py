740711a8f930986ec49f66ddfc83e164cd63478c
saverioruffo04-stack <saverioruffo04@gmail.com>
2026-09-17 17:41:40 +0200

    Committing 1 item from workspace 8d20343b-56af-4125-b338-3e5e936018e9

--- fabric-src/Query Products.Notebook/notebook-content.py
+++ fabric-src/Query Products.Notebook/notebook-content.py
@@ -8,9 +8,14 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse_name": "",
-# META       "default_lakehouse_workspace_id": "",
-# META       "known_lakehouses": []
+# META       "default_lakehouse": "2e03db71-78ca-4268-8b41-d0962bae5c49",
+# META       "default_lakehouse_name": "lh_edu",
+# META       "default_lakehouse_workspace_id": "8d20343b-56af-4125-b338-3e5e936018e9",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "2e03db71-78ca-4268-8b41-d0962bae5c49"
+# META         }
+# META       ]
 # META     },
 # META     "warehouse": {
 # META       "known_warehouses": []
